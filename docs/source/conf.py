a737e247dcae6a6e58a9f525b4a43c3b52889738
Peace Aisosa Osarenren <105794651+LowCodeDataGirl@users.noreply.github.com>
2024-01-15 23:54:16 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Sphinx Quick Start Guide'
+copyright = '2023, Peace Aisosa'
+author = 'Peace Aisosa'
 
 release = '0.1'
 version = '0.1.0'
